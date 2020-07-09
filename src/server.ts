4c93be845cc03bf640611dfbe0c5e82fad65c1ac
Luan <luanfrancavieira@gmail.com>
2020-07-08 21:08:42 -0300

    view image

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 import './database';
+import uploadConfig from './config/upload';
 
 const app = express();
 const port = 3333;
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(port, () => {
