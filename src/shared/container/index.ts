16c367fe8ecbb0654b1ae59bdd4a3200508b91e1
Luan <luanfrancavieira@gmail.com>
2020-09-02 21:52:28 -0300

    save token in db

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,6 +9,11 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IUsersTokensRepository from '@modules/users/repositories/IUserTokenRepository';
+import UsersTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 container.registerSingleton<IAppointmentsRepository>('AppointmentsRepository', AppointmentsRepository);
 
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+
+container.registerSingleton<IUsersTokensRepository>('UsersRepository', UsersTokensRepository);
