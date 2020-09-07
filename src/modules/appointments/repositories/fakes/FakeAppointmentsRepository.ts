324e91144c0474af5e95a2dadfb10735232c603c
Luan <luanfrancavieira@gmail.com>
2020-09-07 15:02:23 -0300

    list available times

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,9 +1,10 @@
 import { uuid } from 'uuidv4';
-import { isEqual, getMonth, getYear } from 'date-fns';
+import { isEqual, getMonth, getYear, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllMonthFromProviderDTO';
+import IFindAllDayFromProviderDTO from '@modules/appointments/dtos/IFindAllDayFromProviderDTO';
 
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
@@ -33,6 +34,17 @@ class AppointmentsRepository implements IAppointmentsRepository {
     public async findAllInMonthFromProvider({ provider_id, month, year }: IFindAllMonthFromProviderDTO): Promise<Appointment[]> {
         const appointments = this.appointments.filter(appointment => 
             appointment.provider_id === provider_id &&
+            getMonth(appointment.date) + 1 === month &&
+            getYear(appointment.date) === year
+        );
+
+        return appointments;
+    }
+
+    public async findAllInDayFromProvider({ provider_id, day, month, year }: IFindAllDayFromProviderDTO): Promise<Appointment[]> {
+        const appointments = this.appointments.filter(appointment => 
+            appointment.provider_id === provider_id &&
+            getDate(appointment.date) === day &&
             getMonth(appointment.date) + 1 === month &&
             getYear(appointment.date) === year
         );
---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,53 @@
+import { injectable, inject } from 'tsyringe';
+import { getHours } from 'date-fns';
+
+import AppError from '@shared/errors/AppError';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+    provider_id: string;
+    day: number;
+    month: number;
+    year: number;
+}
+
+type IResponse = Array<{
+    hour: number;
+    available: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+    constructor(
+        @inject('AppointmentsRepository')
+        private appointmentsRepository: IAppointmentsRepository,
+    ) {}
+
+    public async execute({ provider_id, day, month, year }: IRequest): Promise<IResponse> {
+        const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
+            provider_id, 
+            day, 
+            month, 
+            year,
+        });
+
+        const hourStart = 8;
+        const eachHourArray = Array.from(
+            { length: 10 }, 
+            (value, index) => index + hourStart,
+        );
+
+        const availability = eachHourArray.map(hour => {
+            const hasAppointmentInHour = appointments.find(appointment => getHours(appointment.date) === hour);
+
+            return {
+                hour,
+                available: !hasAppointmentInHour,
+            };
+        });
+
+        return [{ hour: 8, available: true }];
+    }
+}
+
+export default ListProviderMonthAvailabilityService;
