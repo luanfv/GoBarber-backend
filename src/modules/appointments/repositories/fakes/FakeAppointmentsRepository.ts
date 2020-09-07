683bb08638fa9a2e61982a566294532ff2cfed75
Luan <luanfrancavieira@gmail.com>
2020-09-07 14:04:59 -0300

    filter for month

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,8 +1,9 @@
 import { uuid } from 'uuidv4';
-import { isEqual } from 'date-fns';
+import { isEqual, getMonth, getYear } from 'date-fns';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllMonthFromProviderDTO';
 
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
@@ -27,6 +28,16 @@ class AppointmentsRepository implements IAppointmentsRepository {
         this.appointments.push(appointment);
 
         return appointment;
+    }
+
+    public async findAllInMonthFromProvider({ provider_id, month, year }: IFindAllMonthFromProviderDTO): Promise<Appointment[]> {
+        const appointments = this.appointments.filter(appointment => 
+            appointment.provider_id === provider_id &&
+            getMonth(appointment.date) + 1 === month &&
+            getYear(appointment.date) === year
+        );
+
+        return appointments;
     }
 }
 
