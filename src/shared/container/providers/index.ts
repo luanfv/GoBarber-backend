83807106fe8402641a5ca39a3df829dd71caf26e
Luan França Vieira <luanfrancavieira@gmail.com>
2020-09-14 20:15:47 -0300

    config aws mail

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,10 +1,12 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
@@ -13,4 +15,9 @@ container.registerSingleton<IStorageProvider>('StorageProvider', DiskStorageProv
 
 container.registerSingleton<IMailTemplateProvider>('MailTemplateProvider', HandlebarsMailTemplateProvider);
 
-container.registerInstance<IMailProvider>('MailProvider', container.resolve(EtherealMailProvider));
+container.registerInstance<IMailProvider>(
+    'MailProvider', 
+    mailConfig.driver === 'ethereal' 
+    ? container.resolve(EtherealMailProvider) 
+    : container.resolve(SESMailProvider),
+);
