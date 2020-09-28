4263f496850400c99169a09b3ae75ac4e00c5407
Luan França Vieira <luanfrancavieira@gmail.com>
2020-09-28 19:51:07 -0300

    cache provider list

--- src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -2,7 +2,7 @@ import { NoiseReducerSpatialFilterSettings } from "aws-sdk/clients/mediaconvert"
 import { NullableBoolean } from "aws-sdk/clients/xray";
 
 export default interface ICacheProvider {
-    save(key: string, value: string): Promise<void>;
-    recover(key: string): Promise<string | null>;
+    save(key: string, value: any): Promise<void>;
+    recover<T>(key: string): Promise<T | null>;
     invalidate(key: string): Promise<void>;
 }
